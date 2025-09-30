db03764807da1e9918e74686ebcd171e96b9f06c
Tr0im0n <129655960+Tr0im0n@users.noreply.github.com>
2025-09-30 15:34:33 +0200

    Drew Background
    Still need the steps

--- src/Separation/McCabeThiele.py
+++ src/Separation/McCabeThiele.py
@@ -2,69 +2,86 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-
-def equilibrium_curve(x, alpha):
-    return alpha*x / ((alpha-1)*x + 1)
+from tools import lines
 
 
-def equilibrium_curve_inverse(y, alpha):
-    return y / (alpha - y*(alpha-1))
+class McCabeThiele:
+    def __init__(self, xf=0.68, xd=0.93, xb=0.04, alpha=1.85, r=3, b=10):
+        self.xf = xf
+        self.xd = xd
+        self.xb = xb
+
+        self.alpha = alpha
+        self.r = r
+        self.b = b
+
+        self.rectifying_line = r/(r+1), xd/(r+1)
+        self.stripping_line = (b+1)/b, -xb/b
+        self.q_point = lines.intersect(*self.rectifying_line, *self.stripping_line)
+
+        self.xs = np.arange(0, 1.01, 0.01)
+        self.eq_curve = self.make_equilibrium_curve(self.xs)
+
+        self.background = None
+
+    def make_equilibrium_curve(self, x):
+        return self.alpha*x / ((self.alpha-1)*x + 1)
+
+    def make_equilibrium_curve_inverse(self, y):
+        return y / (self.alpha - y*(self.alpha-1))
+
+    def build_figure(self):
+        fig, ax = plt.subplots(figsize=(8, 6), dpi=120)
+
+        ax.set_xlim(0., 1.)
+        ax.set_ylim(0., 1.)
+
+        line, = ax.plot([0, 0], [0, 0], 'r-')
+
+        self.draw_background(fig, ax)
+
+        ax.draw_artist(line)
+
+        def on_reshape(event):
+            self.background = fig.canvas.copy_from_bbox(ax.bbox)
+
+        def on_mouse_move(event):
+            if (not event.inaxes) or self.background is None:
+                return 0
+
+            fig.canvas.restore_region(self.background)
+
+            line.set_data([0, event.xdata], [0, event.ydata])
+            ax.draw_artist(line)
+
+            fig.canvas.blit(ax.bbox)  # ax or fig
+            return 1
+
+        cid1 = fig.canvas.mpl_connect('draw_event', on_reshape)
+        cid2 = fig.canvas.mpl_connect('motion_notify_event', on_mouse_move)
+
+        plt.show()
+
+    def draw_background(self, fig, ax):
+        ax.cla()
+
+        ax.plot([0, 1], [0, 1])
+        ax.plot(self.xs, self.eq_curve)
+        ax.plot([self.xb, self.q_point[0]], [self.xb, self.q_point[1]])
+        ax.plot([self.q_point[0], self.xd], [self.q_point[1], self.xd])
+        ax.plot([self.xf, self.q_point[0]], [self.xf, self.q_point[1]])
+
+        ax.text(self.xf, self.xf, "Feed", ha='left', va='top', fontsize=12)
+        ax.text(self.xb, self.xb, "Bottom", ha='left', va='top', fontsize=12)
+        ax.text(self.xd, self.xd, "Distillate", ha='left', va='top', fontsize=12)
+        # ax.text(*self.q_point, "Q point", ha='left', va='top', fontsize=16)
+
+        self.background = fig.canvas.copy_from_bbox(ax.bbox)
 
 
-alpha1 = 2.4
-x1 = np.arange(0, 1.01, 0.01)
-y1 = equilibrium_curve(x1, alpha1)
-
-fig, ax = plt.subplots(figsize=(8, 6), dpi=120)
-
-ax.set_xlim(0., 1.)
-ax.set_ylim(0., 1.)
-
-background = None
-line, = ax.plot([0, 0], [0, 0], 'r-')
 
 
-def draw_background():
-    ax.cla()
-
-    ax.plot([0, 1], [0, 1])
-    ax.plot(x1, y1)
-
-    global background
-    background = fig.canvas.copy_from_bbox(ax.bbox)
-    ax.draw_artist(line)
-
-
-def on_reshape(event):
-    # draw_background()
-    global background
-    background = fig.canvas.copy_from_bbox(ax.bbox)
-
-
-def on_mouse_move(event):
-    if not event.inaxes:
-        return 0
-
-    if background is None:
-        return 1
-
-    fig.canvas.restore_region(background)
-
-    x, y = event.xdata, event.ydata
-    line.set_data([0, x], [0, y])
-    ax.draw_artist(line)
-
-    fig.canvas.blit(ax.bbox)   # ax or fig
-    return 2
-
-
-draw_background()
-
-cid1 = fig.canvas.mpl_connect('draw_event', on_reshape)
-cid2 = fig.canvas.mpl_connect('motion_notify_event', on_mouse_move)   # 'button_press_event'
-
-
-
-plt.show()
+mct1 = McCabeThiele()
+mct1.build_figure()
 
 
